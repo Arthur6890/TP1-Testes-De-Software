9e0e065ffb389ff153322e7215dee66c5ca20e0a
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2023-11-01 19:42:03 -0300

    correcao de testes e alteração em package.json

--- tests/mocks/FlightObject.ts
+++ tests/mocks/FlightObject.ts
@@ -1,19 +1,19 @@
 import { IFlight, Status } from "../../Interfaces/IFlight";
 
 export const flightOne = {
-  id: "1",
-  pilot: "Arthur",
-  origin: {
-    city: "Belo Horizonte",
-    state: "MG",
-    contry: "Brasil",
-  },
-  destination: {
-    city: "Rio de Janeiro",
-    state: "RJ",
-    contry: "Brasil",
-  },
-  departure: new Date(),
-  status: Status.PENDING,
-  ocupation: 100,
+	id: "1",
+	pilot: "Arthur",
+	origin: {
+		city: "Belo Horizonte",
+		state: "MG",
+		country: "Brasil",
+	},
+	destination: {
+		city: "Rio de Janeiro",
+		state: "RJ",
+		country: "Brasil",
+	},
+	departure: new Date(),
+	status: Status.PENDING,
+	ocupation: 100,
 } as IFlight;
